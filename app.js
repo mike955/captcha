a346dd206e36ce8988f0568c0aad76de25fdec87
zza00000 <cailx@getui.com>
2019-05-08 10:00:46 +0800

    add generate and validate

---
+++ app.js
@@ -0,0 +1,66 @@
+const svgCaptcha = require('svg-captcha');
+const crypto = require('crypto');
+const Redis = require('ioredis');
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const app = new Koa();
+const captcha_conf = require('./conf');
+
+const redis = new Redis({
+  port: 6379,
+  host: '192.168.56.101',
+  db: 12,
+});
+
+if (redis.connector.connecting == true) console.log('connect redis failed');
+console.log('connect redis success');
+
+app.use(bodyParser());
+app.use(async ctx => {
+  let req = ctx.req;
+  if (req.url === '/getCaptcha') return await getCaptcha(ctx);
+  if (req.url === '/validateCaptcha') return await validateCaptcha(ctx);
+  ctx.status = 404;
+  ctx.body = 'Not Found';
+});
+
+app.listen(3000, err => {
+  if (err) throw new Error(err);
+  console.log('server listen 3000');
+});
+
+async function getCaptcha(ctx) {
+  let type = captcha_conf.type;
+  let cap = await generateCaptcha(captcha_conf[type], type);
+  // 增加 存储到 redis 步骤
+  let { pid, text, svg } = cap;
+  let set_res = await redis.set(pid, text);
+  if (set_res != 'OK') console.log('set cap to redis failed');
+  console.log('set cap to redis success');
+  ctx.body = { pid, svg };
+}
+
+async function validateCaptcha(ctx) {
+  let req = ctx.request.body;
+  let { pid, code } = req;
+  let res = { data: false };
+  if (pid == undefined || code == undefined) throw new Error('request body error');
+  let pid_code = await redis.get(pid);
+  if (pid_code == code) {
+    res.data = true;
+    process.nextTick(async () => {
+      let del_res = await redis.del(pid);
+      del_res == 1 ? console.log(`delete key ${pid} failed`) : console.log(`delete key ${pid} success`);
+    });
+  }
+  ctx.body = res;
+}
+
+async function generateCaptcha(conf, type) {
+  let c = type == 'pic' ? svgCaptcha.create(conf) : svgCaptcha.createMathExpr(conf);
+  let pid = crypto
+    .createHash('md5')
+    .update(c.data)
+    .digest('hex');
+  return { pid, text: c.text, svg: c.data };
+}
---
+++ conf.js
@@ -0,0 +1,16 @@
+module.exports = {
+  type: 'math',
+  pic: {
+    size: 4,
+    ignoreChars: '0o1i',
+    noise: 4,
+    color: true,
+    background:  '#cc9966'
+  },
+  math: {
+    noise: 4,
+    color: true,
+    background:  '#cc9966'
+  }
+  
+}
